a3bc01144bdd38f9f876fc3a524942d20796abba
ashishsarwal <ashish.sarwal@gmail.com>
2023-12-30 22:42:31 +0530

    Deleted Backend

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -2,7 +2,8 @@
 // Function to get the domain either dev or production
 
 function getAPIDomain(){
-    return 'http://localhost:3000';
+    //return 'http://localhost:3000';
+    return 'https://thought-evolution-backend.onrender.com';
 }
 
 function getIntParam(paramName){
